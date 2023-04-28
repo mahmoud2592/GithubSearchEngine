5df387034f280e2b580d61591220754f54f8000c
Mahmoud <mahmoudhussien25@gmail.com>
2023-04-28 11:51:05 +0300

    re-design DB tables

--- app/models/search_filter.rb
+++ app/models/search_filter.rb
@@ -3,9 +3,9 @@ class SearchFilter < ApplicationRecord
   belongs_to :filter
   belongs_to :filter_value
 
-  validates :search_id, presence: true
-  validates :filter_id, presence: true
-  validates :filter_value_id, presence: true
+  # validates :search_id, presence: true
+  # validates :filter_id, presence: true
+  # validates :filter_value_id, presence: true
 
   # Add an index to the search_id column
   # index :search_id
@@ -16,6 +16,6 @@ class SearchFilter < ApplicationRecord
   # Add an index to the filter_value_id column
   # index :filter_value_id
 
-  scope :with_filter_value, ->(filter_value_id) { where(filter_value_id: filter_value_id) }
+  # scope :with_filter_value, ->(filter_value_id) { where(filter_value_id: filter_value_id) }
 
 end
